c9377b95644aaa87ab1d0f9e615b4199e0ea8c77
Elias Grey <eliasgrey@Isaiahs-MacBook-Pro.local>
2022-11-09 09:46:08 -0800

    lunchtime

---
+++ src/reducers/productSlice.js
@@ -0,0 +1,31 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+//Thunk to fetch all
+export const fetchProducts = createAsyncThunk("fetchProducts", async () => {
+  try {
+    const { data } = await axios.get("/api/products");
+    return data;
+  } catch (error) {
+    console.log("Couldn't fetch products!", err);
+  }
+});
+
+const initialState = { products: [] };
+
+const productSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {},
+
+  extraReducers: (builder) => {
+    builder.addCase(fetchExamples.fulfilled, (state, action) => {
+      return { products: action.payload };
+    });
+  },
+});
+
+export const selectProducts = (state) => {
+  return state.products.products;
+};
+export default productSlice;
