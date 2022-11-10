7ad7a820dc50b7c6cfbd2b1b63ce58c871fdc3e0
gowithzflow <zskateray@gmail.com>
2022-11-10 10:11:46 -0500

    changed cart selector

--- src/reducers/cartSlice.js
+++ src/reducers/cartSlice.js
@@ -18,7 +18,7 @@ const cartSlice = createSlice({
 });
 
 export const cartSelect = (state) => {
-    return state.products.products
+    return state.cart.products
 };
 
 export const { addToCart } = cartSlice.actions;
