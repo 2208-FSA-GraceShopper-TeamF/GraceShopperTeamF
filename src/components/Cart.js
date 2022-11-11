40d21978dd0522c6b2b8a2e75e0602ac7e247e9f
Elias Grey <eliasgrey@Isaiahs-MacBook-Pro.local>
2022-11-11 06:57:07 -0800

    css changes

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,56 +3,68 @@ import { useDispatch, useSelector } from "react-redux";
 import { cartSelect, removeFromCart } from "../reducers/cartSlice";
 
 const Cart = () => {
-    let cartItems = useSelector(cartSelect);
+  let cartItems = useSelector(cartSelect);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const remove = ( product) =>{
-        console.log('removing:', product.id);
-        const data = cartItems.filter((item) => item.id !== product.id);
-        console.log('new cart:', data);
+  const remove = (product) => {
+    console.log("removing:", product.id);
+    const data = cartItems.filter((item) => item.id !== product.id);
+    console.log("new cart:", data);
 
-        dispatch(removeFromCart(data));
-       
-       
-    };
-    
-    // function decreaseAmount(){
-        
-    // }
-    return (
-        <div className="cart-items">
-            <table>
-                <tbody>
-                <h2 className="cart-items-header">Cart Items</h2>
+    dispatch(removeFromCart(data));
+  };
 
-                    {
-                        cartItems.map((items) => {
-                            return (
-                                <tr key={items.id}>
-                                    <td>{items.name}</td>
-                                    <td><img src={items.imageUrl} width="100px" height="100px" /></td>
-                                    <td>{items.price}</td>
-                                    <td>
-                                        {/* <button onClick={() => {
+  // function decreaseAmount(){
+
+  // }
+  return (
+    <div className="cart-items">
+      <table>
+        <tbody>
+          <h2 className="cart-items-header">Cart Items</h2>
+
+          {cartItems.map((items) => {
+            return (
+              <tr key={items.id}>
+                <td>{items.name}</td>
+                <td>
+                  <img src={items.imageUrl} width="100px" height="100px" />
+                </td>
+                <td>{items.price}</td>
+                <td>
+                  {/* <button onClick={() => {
                                             if ({items.quantity} <= 1){
                                                 disable button
                                             }    
                                         }}><</button> */}
-                                        {/* {items.quantity} */}
-                                        
-                                        <input name="count" type='number' min='1' max={items.inventory} defaultValue='1'></input> {/**adjust to account for 0 item quantity */}
-                                    </td>
-                                    <td><button onClick={()=> {remove(items)}}>X</button></td>
-                                </tr>
-                            )
-                        })
-                    }
-                    <button className="cart-buy-btn">Checkout</button>
-                </tbody>
-            </table>
-        </div>
-    )
+                  {/* {items.quantity} */}
+                  <input
+                    name="count"
+                    type="number"
+                    min="1"
+                    max={items.inventory}
+                    defaultValue="1"
+                  ></input>{" "}
+                  {/**adjust to account for 0 item quantity */}
+                </td>
+                <td>
+                  <button
+                    onClick={() => {
+                      remove(items);
+                    }}
+                  >
+                    X
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+          <button className="cart-buy-btn">Checkout</button>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Cart;
