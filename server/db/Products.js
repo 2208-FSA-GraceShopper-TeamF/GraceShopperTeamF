7c43212ed28c3f4c62d88522bd1ffc123d2de56e
Kevin <jkwk539@gmail.com>
2022-11-11 15:47:39 -0500

    fixed dupe and updated count

--- server/db/Products.js
+++ server/db/Products.js
@@ -17,6 +17,14 @@ const Products = db.define("Products", {
       min: 0.01,
     },
   },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      min: 0,
+    }
+  },
   inventory: {
     type: Sequelize.INTEGER,
     allowNull: false,
