68c674a1870956593d5ee10664ed0aa07ae7806b
Elias Grey <eliasgrey@Isaiahs-MacBook-Pro.local>
2022-11-12 14:25:38 -0800

    started the home page

--- public/bundle.js
+++ public/bundle.js
@@ -7980,7 +7980,7 @@ var Checkout = function Checkout() {
     className: "checkout-total"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("h3", {
     className: "checkout-total"
-  }, "Total: $", totalPriceRdc))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("form", {
+  }, "Total: $", totalPriceRdc.toFixed(2)))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("form", {
     className: "checkout-form"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("label", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("p", null, " First Name:"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("input", {
     className: "checkout-form-input",
@@ -8019,20 +8019,26 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/dist/index.js");
 
 
 var Home = function Home() {
   return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement((react__WEBPACK_IMPORTED_MODULE_0___default().Fragment), null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
+    className: "home-body"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("h2", null, "Collections"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
     className: "collections"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
-    className: "hiro-img"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("img", {
-    className: "{hiro-img}",
-    src: ""
-  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__.NavLink, {
-    to: "/products"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("button", null, "Shop Now")))));
+    className: "collection"
+  }, "10 oz. Ringneck"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
+    className: "collection"
+  }, "12 oz. Wine Tumbler"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
+    className: "collection"
+  }, "15 oz. Camp Mug"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
+    className: "collection"
+  }, "20 oz. Ringneck"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
+    className: "collection"
+  }, "30 oz. Ringneck"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", {
+    className: "collection"
+  }, "32 oz Water Bottle"))));
 };
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Home);
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,15 @@ import { NavLink } from "react-router-dom";
 const Home = () => {
   return (
     <>
-      <div className={"collections"}>
-        <div className={"hiro-img"}>
-          <img className="{hiro-img}" src={""}></img>
-          <NavLink to="/products">
-            <button>Shop Now</button>
-          </NavLink>
+      <div className="home-body">
+        <h2>Collections</h2>
+        <div className="collections">
+          <div className="collection">10 oz. Ringneck</div>
+          <div className="collection">12 oz. Wine Tumbler</div>
+          <div className="collection">15 oz. Camp Mug</div>
+          <div className="collection">20 oz. Ringneck</div>
+          <div className="collection">30 oz. Ringneck</div>
+          <div className="collection">32 oz Water Bottle</div>
         </div>
       </div>
     </>
