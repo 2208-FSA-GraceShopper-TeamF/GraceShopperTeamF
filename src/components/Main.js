a16c2032a75243b225ad6275a92c67e663a3e771
gowithzflow <zskateray@gmail.com>
2022-11-15 16:15:30 -0500

    Fixed logged in feature, adds Admin link to Nav, changed LogIn->Logout

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,29 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { AllProducts, SingleProduct, Cart, Home, Checkout, LogIn, currentUser, Admin} from "./index";
+import { AllProducts, SingleProduct, Cart, Home, Checkout, LogIn, Admin} from "./index";
 import { Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { currentUser } from "../reducers/UserSlice";
 
 
 const Main = () => {
+  const user = useSelector(currentUser);
+  console.log(user);
+  let adminPage;
+  let loggedIn = <NavLink className={"navLinks"} to="/login">
+  Login
+</NavLink>;
+  if(user ){
+    loggedIn=<NavLink className={"navLinks"} to="/home">
+    Logout
+  </NavLink>;
+    if(user.username === 'admin'){
+    adminPage = <NavLink className={"navLinks"} to="/admin">
+    Admin
+  </NavLink>
+  }
+  }
+  
   return (
     <div className="bodyClass">
       <nav className="main-navbar">
@@ -13,12 +32,11 @@ const Main = () => {
           <NavLink className={"navLinks"} to="/home">
             Home
           </NavLink>
+          {adminPage? adminPage:''}
           <NavLink className={"navLinks"} to="/about">
             About
           </NavLink>
-          <NavLink className={"navLinks"} to="/login">
-            Login
-          </NavLink>
+          {loggedIn}
           <NavLink className={"navLinks"} to="/cart">
             Cart
           </NavLink>
